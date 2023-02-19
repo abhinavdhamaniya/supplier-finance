5d8fa7de4a6dc3073d90e2818c91ac7ff442f6e8
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2023-02-19 15:15:03 +0530

    Initial commit

---
+++ src/main/java/com/abhinav/supplierfinance/SupplierFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.supplierfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierFinanceApplication.class, args);
+	}
+
+}
